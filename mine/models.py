e53afb0dcace8a0d9af8d23298a68d09917fcf8f
GonzaloWang <15890772752@163.com>
2020-03-27 22:29:00 +0800

    second commit

--- mine/models.py
+++ mine/models.py
@@ -4,12 +4,14 @@ from django.db import models
 from accounts.models import User
 from mall.models import Product
 
+ORDER_STATUS_INIT = 10
 ORDER_STATUS_SUBMIT = 11
 ORDER_STATUS_PAIED = 12
 ORDER_STATUS_SENT = 13
 ORDER_STATUS_DONE = 14
 ORDER_STATUS_DELETED = 15
 ORDER_STATUS_CHOICES = (
+    (ORDER_STATUS_INIT, '购物车'),
     (ORDER_STATUS_SUBMIT, '已经生成订单'),
     (ORDER_STATUS_PAIED, '已经付款'),
     (ORDER_STATUS_SENT, '已经发货'),
@@ -21,10 +23,7 @@ ORDER_STATUS_CHOICES = (
 class Order(models.Model):
     order_id = models.CharField("订单唯一ID", max_length=32)
     user_id = models.ForeignKey(User, related_name='orders')
-    product = models.ForeignKey(Product)
     address = models.CharField("收货地址", max_length=256)
-    create_time = models.DateTimeField("下单时间", auto_now_add=True)
-    update_time = models.DateTimeField("购买日期", auto_now=True)
     total_price = models.IntegerField("总价")
     status = models.SmallIntegerField("订单状态",
                                       choices=ORDER_STATUS_CHOICES,
@@ -33,3 +32,20 @@ class Order(models.Model):
 
     class Meta:
         db_table = 'mine_order'
+
+
+class Cart(models.Model):
+    user_id = models.ForeignKey(User, related_name='carts')
+    product = models.ForeignKey(Product)
+    order = models.ForeignKey(Order, verbose_name='订单', null=True)
+    count = models.PositiveIntegerField("购买数量")
+    amount = models.FloatField('总额')
+    create_time = models.DateTimeField("下单时间", auto_now_add=True)
+    update_time = models.DateTimeField("购买日期", auto_now=True)
+    status = models.SmallIntegerField('状态',
+                                      choices=ORDER_STATUS_CHOICES,
+                                      default=ORDER_STATUS_INIT)
+
+    class Meta:
+        db_table = 'mine_cart'
+        unique_together = ("user_id", "product", "order")
